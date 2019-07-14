f5222991aa667565f2760c0c299bd4e00d3ac43e
AugustVParnate <52869273+AugustVParnate@users.noreply.github.com>
2019-07-15 02:25:57 +0300

    Add constructor, getters

--- src/com/seriozhkina/tetrahedron/Tetrahedron.java
+++ src/com/seriozhkina/tetrahedron/Tetrahedron.java
@@ -6,5 +6,28 @@ public class Tetrahedron {
         private Point c;
         private Point d;
 
+
+        public Tetrahedron(Point a, Point b, Point c, Point d) {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+                this.d = d;
+        }
+
+        public Point getA() {
+                return a;
+        }
+
+        public Point getB() {
+                return b;
+        }
+
+        public Point getC() {
+                return c;
+        }
+
+        public Point getD() {
+                return d;
+        }
 }
 
